1059d1642205e0b2fac566a04d797e7893d1d414
sunaina rishi <sunaina.d.rishi@gmail.com>
2023-05-08 16:51:24 +0100

    TDD use of user input values

--- resources/example-3/messageView.js
+++ resources/example-3/messageView.js
@@ -13,18 +13,23 @@ class MessageView {
   }
 
   displayMessage() {
+    const message = document.querySelector('#message-input').value;
+
     const messageEl = document.createElement('div');
-    messageEl.innerText = 'This message displayed by JavaScript';
+    messageEl.innerText = message;
     messageEl.id = 'message';
 
     //would be interesting to change this so it appears before the button using method rather than altering html file
     document.querySelector('#main-container').append(messageEl);
     
     console.log('Thanks for clicking me!');
+    // To set the user input field to empty again once the user has clicked button
+    document.querySelector('#message-input').value = '';
   }
 
+  // Can get uncaught type error when the message is null
   hideMessage() {
-
+    
     document.querySelector('#message').remove();
     
     console.log('Removed message');
--- resources/example-3/messageView.test.js
+++ resources/example-3/messageView.test.js
@@ -13,8 +13,13 @@ describe('MessageView', () => {
   it('clicks the button', () => {
     const view = new MessageView();
     const buttonEl = document.querySelector('#show-message-button');
+    const inputEl = document.querySelector('#message-input');
+    inputEl.value = 'Hello, this is my message. Meow!'
+
     buttonEl.click();
     expect(document.querySelector('#message')).not.toBeNull();
+    expect(document.querySelector('div#message').innerText).toBe('Hello, this is my message. Meow!')
+    expect(inputEl.value).toBe('')
   });
 
   it('clicks the hide message button', () => {
