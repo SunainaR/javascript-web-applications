caef1c4f5c201a595cbd5ce65a06855a647449be
sunaina rishi <sunaina.d.rishi@gmail.com>
2023-05-20 19:49:28 +0100

    Phase 3 Exercise 1 - fetch

---
+++ resources/example-4/githubView.js
@@ -0,0 +1,29 @@
+class GithubView {
+  constructor(model, client) {
+    this.model = model;
+    this.client = client;
+
+    const submitButtonEl = document.querySelector('#submit-button');
+    const repoInputEl = document.querySelector('#repo-name-input');
+
+    submitButtonEl.addEventListener('click', () => {
+      const repoName = repoInputEl.value;
+
+      this.client.getRepoInfo(repoName, repoData => {
+        console.log(repoData)
+        this.display(repoData);
+      });
+      
+    });
+  }
+
+  display(repoData) {
+    document.querySelector('#repo-name').innerText  = repoData.full_name
+    document.querySelector('#repo-description').innerText  = repoData.description
+    document.querySelector('#repo-img').src = repoData.organization.avatar_url
+
+
+  }
+}
+
+module.exports = GithubView;
